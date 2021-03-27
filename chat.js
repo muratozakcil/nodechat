97ed965f7c837514879a2615978ba082a81f3f7b
Murat Özakçil <murat@sanalyer.com>
2021-03-28 00:29:30 +0300

    socket.io connection setting

--- chat.js
+++ chat.js
@@ -1,11 +1,27 @@
-const http = require("http");
-const express = require("express");
-const app = express();
-const server = http.createServer(app);
-const io = require('socket.io')(server);
+const http = require("http")
+const express = require("express")
+const app = express()
+const server = http.createServer(app)
+const io = require('socket.io')(server)
+
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'))
 
 app.get("/", function (request,response){
     response.sendFile('./index.html',{root : __dirname})
+
+})
+
+io.on('connection', function (socket){
+    console.log("Bir kullanıcı bağlandı")
+
+    socket.on('newmessage', function (msg){
+       io.emit('mesajvar', msg)
+    })
+
+    socket.on('disconnect', function (){
+        console.log("Bir kullanıcı ayrıldı")
+    })
 
 })
 
